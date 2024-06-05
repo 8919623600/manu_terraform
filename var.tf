4ece56167ed6fc665dcc194deab5367d588edf3d
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-06 01:36:22 +0530

    auto push

--- var.tf
+++ var.tf
@@ -10,4 +10,12 @@ output "sample_output" {
     value = "value is ${var.sample}"
 }
 
-#testing 
+
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+#whenever if any variable is declared inside quotes we have to give dollar symbol 
