069a42823fba4ce10864ffc4851a7c3df56ff559
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-11 16:58:20 +0530

    autopush

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,21 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_instance" "ec2" {
+  ami                     = "ami-0174d086840117932"
+  instance_type           = "t2.micro"
+
+  tags = {
+    Name = "Terraform instance"
+  }
+}
+ 
+
 output "cidr_block" {
   value = aws_vpc.vpc.cidr_block
+  }
+
+output "instance_name" {
+  value = aws_instance.ec2.id
   
 }
