00128c9af0dbc0f6d2c4f7573fc2936d71656d35
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-11 18:04:18 +0530

    autopush

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,3 @@
-provider "aws" {}
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-devops29master"
-    key    = "dev/terraform/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 
 
 resource "aws_vpc" "vpc" {
