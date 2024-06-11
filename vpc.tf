41a66310680dfeeb37fb89a86821be453196f089
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-11 18:29:00 +0530

    autopush

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
 
 
-# resource "aws_vpc" "vpc" {
-#   cidr_block = "172.16.0.0/16"
+resource "aws_vpc" "vpc" {
+  cidr_block = "172.16.0.0/16"
 
-#   tags = {
-#     Name = "tf-example"
-#   }
-# }
+  tags = {
+    Name = "tf-example"
+  }
+}
 
 resource "aws_instance" "ec2" {
   ami                     = "ami-0174d086840117932"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  = aws_security_group.allow_tls.id
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "Terraform instance"
