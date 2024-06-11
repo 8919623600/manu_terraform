7bc2a04885d4358b17f1c7021be0d6b7d3ef8260
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-11 18:33:31 +0530

    autopush

---
+++ ec2.tf
@@ -0,0 +1,39 @@
+
+resource "aws_instance" "ec2" {
+  ami                     = "ami-0174d086840117932"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.my_sg.id]
+
+  tags = {
+    Name = "Terraform instance"
+  } 
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+ 
+
+output "instance_name" {
+  value = aws_instance.ec2.instance_type
+  
+}
