fb1b083297dcf09f373b32c7c492f96ecb5c4753
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-11 18:27:50 +0530

    autopush

--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,54 @@
 
 
-resource "aws_vpc" "vpc" {
-  cidr_block = "172.16.0.0/16"
+# resource "aws_vpc" "vpc" {
+#   cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
+#   tags = {
+#     Name = "tf-example"
+#   }
+# }
 
 resource "aws_instance" "ec2" {
   ami                     = "ami-0174d086840117932"
   instance_type           = "t2.micro"
+  vpc_security_group_ids  = aws_security_group.allow_tls.id
 
   tags = {
     Name = "Terraform instance"
   } 
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+ 
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
  
 
