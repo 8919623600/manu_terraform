9c9aa4d2aed982e7a620f343904a8e4d9f515c71
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-11 19:09:32 +0530

    autopush

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,16 @@
+#here datasource is defined because it willfetch the properties which are already there in aws
 
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "learn-linux-on-centos7-"
+  owners           = ["851725330688"]
+
+
+
+}
 resource "aws_instance" "ec2" {
-  ami                     = "ami-0174d086840117932"
+  ami                     = data.aws_ami.ami.id    # fetching ami id from datasource
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [aws_security_group.my_sg.id]
 
