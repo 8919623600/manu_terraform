6d361df66f5cee9089c5207270b5875aac13a79d
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-13 01:17:59 +0530

    auto push

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -11,6 +11,7 @@ module "vpc" {
   PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
   ENV                   = var.ENV
+  AZ                    = var.AZ
 
 }
 
