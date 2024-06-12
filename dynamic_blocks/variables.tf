501f93330ce2b9f6f4d1ecf6a8bc582a956e7631
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-12 11:39:48 +0530

    autopush

--- dynamic_blocks/variables.tf
+++ dynamic_blocks/variables.tf
@@ -1,27 +1,27 @@
 variable  "ingress_rule" {
     default = [
-    ingress {
+    {
       description     = "ssh"
       from_port       = 22
       to_port         = 22
       protocol        = "tcp"
       cidr_blocks     = ["0.0.0.0/0"]
   },
-    ingress {
+    {
       description     = "web"
       from_port       = 80
       to_port         = 80
       protocol        = "tcp"
       cidr_blocks     = ["0.0.0.0/0"]
   },
-    ingress {
+     {
       description     = "jenkins"
       from_port       = 8080
       to_port         = 8080
       protocol        = "tcp"
       cidr_blocks     = ["0.0.0.0/0"]
   },
-    ingress {
+     {
       description     = "nexus"
       from_port       = 8081
       to_port         = 8081
