4e003f0f3a574dd4f38b8ed19f1fb058b37240c5
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-12 12:18:02 +0530

    autopush

--- dynamic_blocks/sg.tf
+++ dynamic_blocks/sg.tf
@@ -28,3 +28,44 @@ resource "aws_security_group" "sample_sg" {
     Name = "sg"
   }
 }
+
+resource "aws_security_group" "second_sample_sg" {
+  name        = "my_sg_2"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  dynamic ingress {
+    for_each = var.ingress_rule
+    content {
+    description     = ingress.value["description"]
+    from_port       = ingress.value["from_port"]
+    to_port         = ingress.value["to_port"]
+    protocol        = ingress.value["protocol"]
+    cidr_blocks     = ingress.value["cidr_blocks"]
+    }
+  }
+  dynamic ingress_sample {
+    for_each = ingress_rule_sample
+    content {
+    description     = ingress_sample.value["description"]
+    from_port       = ingress_sample.value["from_port"]
+    to_port         = ingress_sample.value["to_port"]
+    protocol        = ingress_sample.value["protocol"]
+    cidr_blocks     = ingress_sample.value["cidr_blocks"]
+    }
+  }
+   dynamic egress {
+    for_each = var.egress_rule
+    content {
+      description     = egress.value["description"]
+      from_port       = egress.value["from_port"]
+      to_port         = egress.value["to_port"]
+      protocol        = egress.value["protocol"]
+      cidr_blocks     = egress.value["cidr_blocks"]
+    }
+   }
+
+
+  tags = {
+    Name = "sg"
+  }
+}
--- dynamic_blocks/variables.tf
+++ dynamic_blocks/variables.tf
@@ -31,6 +31,40 @@ variable  "ingress_rule" {
     ]
 } 
 
+variable  "ingress_rule_sample" {
+    default = [
+    {
+      description     = "ssh"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  },
+    {
+      description     = "nginx"
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  },
+     {
+      description     = "tomcat"
+      from_port       = 8080
+      to_port         = 8080
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  },
+     {
+      description     = "sonarqube"
+      from_port       = 8081
+      to_port         = 8081
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  }
+    ]
+} 
+
+
 variable egress_rule {
     default = [ {
       description     = "egress all"
