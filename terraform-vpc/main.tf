7b36f0f07350bd285c0971c2d077ac3cbe2eca28
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-14 10:33:36 +0530

    auto push

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   VPC_CIDR              = var.VPC_CIDR
   PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
-    
+  ENV                   = var.ENV
   AZ                    = var.AZ
   DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
   DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
