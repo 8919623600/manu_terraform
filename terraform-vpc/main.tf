de6b4798e7c64c9506a5795c771a540d38594448
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-14 10:24:34 +0530

    auto push

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   VPC_CIDR              = var.VPC_CIDR
   PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
-  ENV                   = var.ENV
+    
   AZ                    = var.AZ
   DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
   DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
@@ -19,3 +19,10 @@ module "vpc" {
 }
 
 
+module "ec2" {
+
+    source        = "./vendor/modules/ec2/"
+    instance_type = var.instance_type
+    ENV           = var.ENV
+
+  }
--- terraform-vpc/vars.tf
+++ terraform-vpc/vars.tf
@@ -1,3 +1,4 @@
+#vpc_variables
 variable "VPC_CIDR" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
@@ -8,3 +9,20 @@ variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_RT_ID" {}
+
+#ec2_variables
+variable "instance_type" {}
+variable "component" {}
+variable "COMPONENTS" {
+    default = {
+        catalouge = {
+            instance_type = "t2.micro"
+        },
+        cart = {
+            instance_type = "t2.medium"
+        },
+        shopping = {
+            instance_type = "t2.micro"
+        }
+    }
+}
