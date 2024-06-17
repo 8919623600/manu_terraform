c9504735ecd4d0693cd98e5e5fdb11e4740c572b
8919623600 <44758097+8919623600@users.noreply.github.com>
2024-06-17 12:03:26 +0530

    Update main.tf

--- terraform-databases/main.tf
+++ terraform-databases/main.tf
@@ -1,12 +1,13 @@
 
-module "mongodb" {
-    source                  = "./vendor/modules/mongodb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+module "docdb" {
+    source                  = "./vendor/modules/docdb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
     ENV                     = var.ENV
-    AZ                      = var.AZ
     DOCDB_ENGINE_VERSION    = var.DOCDB_ENGINE_VERSION
     DOCDB_INSTANCE_COUNT    = var.DOCDB_INSTANCE_COUNT
     DOCDB_INSTANCE_TYPE     = var.DOCDB_INSTANCE_TYPE
     DOCDB_PORT              = var.DOCDB_PORT
+
+ 
 }
 
 # module "mysql" {
